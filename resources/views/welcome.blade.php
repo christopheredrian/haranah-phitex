5a17d03b95980f21fcad85078af0045556f6da3b
Anne Zheng <maryannezheng@gmail.com>
2017-11-06 00:59:44 +0800

    Addded temp photos and initial public page look

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,49 @@
 @extends('layouts.public')
+<style>
+    .caption {
+        background-color: rgba(0, 0, 0, 0);
+        bottom: 0px;
+        color: white;
+        font-size: 1.2em;
+        left: 0px;
+        padding: 10px 15px;
+        position: absolute;
+        transition: 0.5s padding;
+        text-align: center;
+        width: 100%;
+        top: 45%;
 
-<h1>Welcome!</h1>
+    }
+    </style>
+<div class="container" align="center">
+    <div class="row">
+        <div class="col-md-12">
+            <img class="img-responsive" src="/img/banner.jpg" alt="Banner" style="width: 100%; height: 75%">
+            <div class="caption">
+                <h1>WELCOME TO PHITEX</h1>
+                <a class="btn btn-primary" href="/login">Login</a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="img">
+                    <img src="/img/buyer.png" alt="Buyer" style="width:300px; height: 300px;">
+                </a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="img">
+                <img src="/img/buyer.png" alt="Buyer" style="width:300px; height: 300px;">
+                </a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="img">
+                <img src="/img/buyer.png" alt="Buyer" style="width:300px; height: 300px;">
+                </a>
+            </div>
+        </div>
+    </div>
 
-<a class="btn btn-primary" href="/login">Login</a>
+
