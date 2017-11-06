73b97846928daf591ec4f7a27298dcc7ea66553a
zhejaa <maryjane.zhejaa@gmail.com>
2017-11-06 21:17:18 +0800

    minor changes in design

--- resources/views/sellers/index.blade.php
+++ resources/views/sellers/index.blade.php
@@ -1,39 +1,38 @@
 @extends('layouts.app-seller')
 
 @section('content')
+    <img src="http://www.researchassociatesinc.com/RAI/media/RAIMedia/bizinvestigations.jpg" style="width: 100%; max-height: 200px">
+
+    <div class="container">
 <div class="row">
     <div class="col-md-12">
-        <div class="panel panel-default">
-            <img src="http://www.researchassociatesinc.com/RAI/media/RAIMedia/bizinvestigations.jpg" style="width: 100%; max-height: 200px">
-            <div class="panel-heading">Event Name</div>
-            <div class="container">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <p> Description of Event</p>
-                            <p> Event Place</p>
-                            <p> Date of Event</p>
-                        </div>
+        <div class="box box-info">
+            <div class="box-header">Event Name</div>
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p> Description of Event</p>
+                        <p> Event Place</p>
+                        <p> Date of Event</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="col-md-12">
-        <div class="panel panel-default">
-            <div class="panel-heading">List of Interested Buyers</div>
-            <div class="container">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <table>
-                                <thead></thead>
-                            </table>
-                        </div>
+        <div class="box ">
+            <div class="box-header">List of Interested Buyers</div>
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <table>
+                            <thead></thead>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
