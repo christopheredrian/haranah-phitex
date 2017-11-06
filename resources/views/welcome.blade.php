2c922d0c9a361fd0c437c26ed756c8fff6ff769f
Anne Zheng <maryannezheng@gmail.com>
2017-11-06 15:05:50 +0800

    Added imgs for public page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         top: 45%;
         opacity: 0.9;
     }
-    </style>
+</style>
 {{--images are not owned by the developers, all credit belongs to the rightful owner--}}
 <div class="container" align="center">
     <div class="row">
@@ -35,16 +35,37 @@
         </div>
         <div class="col-md-4">
             <div class="img">
-                <img src="/img/buyer.png" alt="Buyer" class="img-thumbnail" style="width:300px; height: 300px;">
+                <img src="/img/seller.png" alt="Seller" class="img-thumbnail" style="width:300px; height: 300px;">
                 </a>
             </div>
         </div>
         <div class="col-md-4">
             <div class="img">
-                <img src="/img/buyer.png" alt="Buyer"  class="img-thumbnail" style="width:300px; height: 300px;">
+                <img src="/img/events.png" alt="Events"  class="img-thumbnail" style="width:300px; height: 300px;">
                 </a>
             </div>
         </div>
     </div>
+</div>
+<div>
+    <div class="card">
+        <div class="card-block">
+            <hr>
+            <p>Philippine Travel Exchange (PHITEX) is a marketing activity conceived in 1996 with the end-goal of increasing tourist arrivals in the country.</p>
 
+            <p>Patterned after ASEAN Tourism Forum (ATF), this event hosts qualified buyers all over the world to participate in a one and half days table top business appointments with Philippine sellers and have actual experience of what the country can offer as a tourism destination through pre/post tours.</p>
 
+            <p>After 5 years’ hibernation, PHITEX made a successful comeback in 2004 and since then became a major annual marketing event that further strengthens the country’s brand image and encourages participants to promote affordable and competitive tourism packages.</p>
+            <p>In 2007, aside from holding it in Cebu for the first time, another component was added in staging of PHITEX, the Educational Seminar. This is a learning activity wherein in industry experts were invited as resource speakers to discuss new trends in tourism marketing as well as marketing strategies in mature and emerging markets.</p>
+
+            <p>In 2015, PHITEX was combined with MICECON, another major marketing activity headed by TPB that aims to consolidate and strengthen various sector involved in M.I.C.E (meeting, Incentive Travel, Conventions and Exhibitions/Events) industry.</p>
+
+            <p>2017 will bring another important and exciting changes to PHITEX:</p>
+            <ul>
+                <li>TRAVEX will be held before the Philippine Travel Mart</li>
+                <li>TRAVEX will be staged for one and half days, yielding a total of 40 pre-arranged business sessions</li>
+                <li>No more pre-tours but Metro Manila half day tours will be offered on the afternoon of 31 August</li>
+            </ul>
+        </div>
+    </div>
+</div>
