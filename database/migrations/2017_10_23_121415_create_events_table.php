916ab4c9bc2f6bdf9e2a1b75e426d2a07b324dce
Christopher Espiritu <christopheredrian@gmail.com>
2017-11-08 23:12:59 +0800

    Revised events to text rather than string

--- database/migrations/2017_10_23_121415_create_events_table.php
+++ database/migrations/2017_10_23_121415_create_events_table.php
@@ -18,7 +18,7 @@ class CreateEventsTable extends Migration
             $table->string('event_place');
             $table->date('event_date');
             $table->string('event_status');
-            $table->string('event_description');
+            $table->text('event_description');
             $table->timestamps();
         });
     }
