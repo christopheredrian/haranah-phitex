33fd2a998ffb320c97394b0fa57018e6845aaa05
Charlene Fama <2150428@slu.edu.ph>
2017-11-09 18:24:08 +0800

    Modified Buyer layouts

--- resources/views/admin/buyers/show.blade.php
+++ resources/views/admin/buyers/show.blade.php
@@ -7,8 +7,6 @@
 @section('content')
     <div class="container">
         <div class="row">
-
-
             <div class="col-md-12">
                 <div class="panel x-panel">
                     <div class="panel-heading">Buyer {{ $buyer->id }}</div>
@@ -50,7 +48,6 @@
                                 </tbody>
                             </table>
                         </div>
-
                     </div>
                 </div>
             </div>
