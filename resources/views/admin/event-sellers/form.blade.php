e201866abae070409046241d2ce446c2ffc68f9b
Jay <2151796@slu.edu.ph>
2017-12-19 01:22:07 +0800

    added jquery for multiple inserts of sellers

--- resources/views/admin/event-sellers/form.blade.php
+++ resources/views/admin/event-sellers/form.blade.php
@@ -14,9 +14,43 @@
         {!! $errors->first('seller_id', '<p class="help-block">:message</p>') !!}
     </div>
 </div>
-
+<div id="container"></div>
+<div class="col-md-offset-4 col-md-6">
+    <button id="delete" class="pull-right" type="button">Delete</button>
+    <button id="addMore" class="pull-right" type="button">Add more</button>
+</div>
 <div class="form-group">
     <div class="col-md-offset-4 col-md-4">
         <input class="btn btn-primary" type="submit" value="{{ $submitButtonText or 'Create' }}">
     </div>
 </div>
+
+@section('scripts')
+    <script>
+        var count = 1;
+        $("#addMore").click(function () {
+            $("#container").append('<div id="seller'+count+'" class="form-group {{ $errors->has('seller_id') ? 'has-error' : ''}}">\n' +
+                '    <label for="seller_id" class="col-md-4 control-label">{{ 'Seller' }}</label>\n' +
+                '    <div class="col-md-6">\n' +
+                '        <select name="seller_id'+count+'" id="seller_id'+count+'" class="form-control">\n' +
+                '            @foreach($sellers as $seller)\n' +
+                '                {{--<option value="{{array_search ($seller_name, $seller_names)}}">{{$seller_name}}</option>--}}\n' +
+                '                <option value="{{ $seller->id }}">{{ $seller->company_name }}</option>\n' +
+                '            @endforeach\n' +
+                '        </select>\n' +
+                '        {{--<input class="form-control" name="seller_id" type="number" id="seller_id" value="{{ $eventseller->seller_id or \'\'}}" >--}}\n' +
+                '        {!! $errors->first('seller_id', '<p class="help-block">:message</p>') !!}\n' +
+                '    </div>\n' +
+                '</div>');
+            count=count+1;
+        });
+        $("#delete").click(function () {
+            if(count>0){
+                count=count-1;
+            }
+            $("#seller"+count).remove();
+
+        });
+
+    </script>
+@endsection
