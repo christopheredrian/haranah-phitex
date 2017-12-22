35a4de6b0f86b116eac7634571e55e1c177f4e2a
zhejaa <maryjane.zhejaa@gmail.com>
2017-12-22 21:12:53 +0800

    made minor UI enhancements for buyer aanad seller.

--- resources/views/auth/passwords/change-password.blade.php
+++ resources/views/auth/passwords/change-password.blade.php
@@ -5,8 +5,8 @@
 @section('content')
     <div class="container">
         <div class="row">
-
-            <div class="col-md-6">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
                 <div class="panel x_panel">
                     <div class="panel-heading">Change Password</div>
                     <div class="panel-body">
@@ -40,6 +40,7 @@
                     </div>
                 </div>
             </div>
+            <div class="col-md-2"></div>
         </div>
     </div>
 @endsection
